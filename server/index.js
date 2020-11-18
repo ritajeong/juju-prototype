ee1a4ce7513caaf19ccdb5b25342e0bff0c194e5
ritajeong <litch0s2@gmail.com>
2020-11-19 02:50:07 +0900

    Repeat installation npm

--- server/index.js
+++ server/index.js
@@ -25,9 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello World! ')
 })
 
-app.get('api/hello', (req, res)=>{
-  res.send("안녕하세요")
-})
+app.get('/api/hello', (req, res)=>  res.send("안녕하세요"))
 
 app.post('/api/users/register', (req, res) =>{
     //회원 가입시 필요한 정보를 클라이언트에서 가져오면 db에 넣음
